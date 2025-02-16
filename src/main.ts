60b3c9bcb2b4b81e931b831cd868c307429b84de
Brunogomes97 <brunogxy10@gmail.com>
2025-02-16 15:05:34 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL_PROD : "http://localhost:3002",
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    },
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
